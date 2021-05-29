46aa729f345d9446bcfe4affd7bb85d7e00cfa80
paulapompeo <paulapompeoc@gmail.com>
2021-05-29 23:30:27 +0200

    home

--- src/components/mainLayout/header/header.js
+++ src/components/mainLayout/header/header.js
@@ -1,40 +1,35 @@
 import React, { Component } from 'react'
+import Logo from '../../../images/read-book.png'
 
 import '../../../css/header/header.css'
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
-          <div className="container px-4 px-lg-5">
-            <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-                    <i className="fas fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ms-auto py-4 py-lg-0">
-                <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-                <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-                <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-                <li className="nav-item"><a className="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
-              </ul>
+      <div className="headerCss">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a className="navbar-brand" href="/">
+            <img alt='logo' src={Logo} className="logo-image" />
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/bookList">BookList</a>
+              </li>
+            </ul>
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="/login">Login</a>
+              <a className="nav-item nav-link" href="#/">Registrazione</a>
             </div>
           </div>
         </nav>
-        <header className="masthead">
-          <div className="container position-relative px-4 px-lg-5">
-            <div className="row gx-4 gx-lg-5 justify-content-center">
-              <div className="col-md-10 col-lg-8 col-xl-7">
-                <div className="site-heading">
-                  <h1>Clean Blog</h1>
-                  <span className="subheading">A Blog Theme by Start Bootstrap</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
       </div>
     )
   }
